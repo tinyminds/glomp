e9073ce5a63e6ca3f691eef9fd1121afa0eba2ef
tinyminds <jenny23@gmail.com>
2015-09-11 18:07:50 +0100

    glomped

---
+++ Assets/BasicGrid/Examples/Scripts/FreezeAxis.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeAxis : MonoBehaviour 
+{
+	public bool freezeX;
+	public bool freezeY;
+	public bool freezeZ;
+
+	private float fixedX = 0;
+	private float fixedY = 0;
+	private float fixedZ = 0;
+	
+	void Start () 
+	{
+		if(freezeX) fixedX = transform.position.x;
+		if(freezeY) fixedY = transform.position.y;
+		if(freezeZ) fixedZ = transform.position.z;
+	}
+
+	void Freeze() 
+	{
+		float newX, newY, newZ;
+		newX = freezeX ? fixedX : transform.position.x;
+		newY = freezeY ? fixedY : transform.position.y;
+		newZ = freezeZ ? fixedZ : transform.position.z;
+		
+		transform.position = new Vector3(newX, newY, newZ);
+	}
+	
+	void Update ()
+	{
+		Freeze();
+	}
+}
---
+++ Assets/BasicGrid/Examples/Scripts/LerpTowardsObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpTowardsObject : MonoBehaviour {
+
+	public Transform targetObject;
+	private Transform lerpObject;
+	public float speed = 5;
+	
+	void Start() 
+	{
+		speed = speed/100;
+		lerpObject = transform;
+	}
+
+	void Update() 
+	{
+		transform.position = Vector3.Lerp(lerpObject.position, targetObject.position, speed);
+	}
+}
